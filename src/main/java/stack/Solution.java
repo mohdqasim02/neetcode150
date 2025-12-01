fc12b6a9b6d69f575661dcb84d27742d7a373362
Mohd Qasim <mohdqasim@Mohds-MacBook-Pro.local>
2025-12-01 15:56:31 +0530

    Solve Valid paranthesis problem

---
+++ src/main/java/stack/Solution.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> parentheses = new Stack<>();
+
+        for (Character c : s.toCharArray()) {
+            try {
+                switch (c) {
+                    case ')' -> {
+                        char openBracket = parentheses.pop();
+                        if (openBracket != '(') return false;
+                    }
+                    case ']' -> {
+                        char openBracket = parentheses.pop();
+                        if (openBracket != '[') return false;
+                    }
+                    case '}' -> {
+                        char openBracket = parentheses.pop();
+                        if (openBracket != '{') return false;
+                    }
+                    default -> parentheses.push(c);
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        return parentheses.isEmpty();
+    }
+}
